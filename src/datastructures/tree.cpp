706ede7e2da13332af64e570495b87c5146a3a9b
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2024-04-30 18:17:29 +0200

    this doesnt work :D

--- src/datastructures/tree.cpp
+++ src/datastructures/tree.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <functional>
 
 namespace DS {
     enum NodeType {
@@ -10,7 +11,7 @@ namespace DS {
     enum Blocked {
         None,
         Left,
-        Right 
+        Split,
     };
 
     template<class T>
@@ -24,13 +25,15 @@ namespace DS {
     };
 
     template<class T>
-    static Tree<T>* generate_tree(std::vector<T> entries) {
-        return DS::generate_tree(entries, 0, (int)entries.size() - 1);
+    static Tree<T>* generate_tree(std::vector<T> entries, std::function<T(T,T)> between) {
+        if (entries.size() == 0) return nullptr;
+
+        return DS::generate_tree(entries, between, 0, (int)entries.size() - 1);
     }
 
     template<class T>
     // Takes an ordered list of items as input
-    static Tree<T>* generate_tree(std::vector<T> entries, int begin, int end) {
+    static Tree<T>* generate_tree(std::vector<T> entries, std::function<T(T, T)> between, int begin, int end) {
         if (begin >= end) {
             Tree<T>* leaf = (Tree<T> *)malloc(sizeof(Tree<T>));
             leaf->type = Leaf;
@@ -42,12 +45,33 @@ namespace DS {
             int middle = (begin + end) / 2 + 1; // +1 for right biased tree 
             Tree<T>* node = (Tree<T> *)malloc(sizeof(Tree<T>));
             node->type = Internal;
-            node->value = entries[middle];
-            node->left = generate_tree(entries, begin, middle - 1);
-            node->right = generate_tree(entries, middle, end);
+            node->left = generate_tree(entries, between, begin, middle - 1);
+            node->right = generate_tree(entries, between, middle, end);
+            node->value = between(node->left->value, node->right->value);
             node->count = node->left->count + node->right->count;
             return node;
         }
+    }
+
+    template<class T> 
+    static void print_tree(Tree<T>* tree, int depth = 0) {
+        for (int i = 0; i < depth; i++) std::cout << "  ";
+        std::cout << ((tree->type == Leaf) ? "leaf: " : "") << tree->value << std::endl;
+        if (tree->type == Internal) {
+            print_tree(tree->left, depth + 1);
+            print_tree(tree->right, depth + 1);
+        }
+    }
+
+    template<class T>
+    // Returns the first node in the tree which is larger than the 
+    // provided query value, determined by the predicate.
+    // Also outputs the entire search path 
+    static Tree<T>* query_tree(Tree<T>* tree, T query_value, std::function<bool(T, T)> pred, std::vector<Tree<T>*>* search_path) {
+        search_path->push_back(tree);
+        if (tree->type == Leaf) return tree;
+        auto target = pred(query_value, tree->value) ? tree->left : tree->right;
+        return query_tree(target, query_value, pred, search_path);
     }
 
     template<class T>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <algorithm>
 #include <CGAL/Simple_cartesian.h>
 #include "datastructures/tree.cpp";
 
@@ -11,29 +12,70 @@ typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_2 Point_2;
 typedef Kernel::Segment_2 Segment_2;
 
-void get_random_points(Point_2 points[], int point_count) {
-    const int min_x = -50;
-    const int max_x = 50;
+void get_random_points(std::vector<Point_2>* points, int point_count) {
+    const int min_x = -1000;
+    const int max_x = 1000;
     std::uniform_real_distribution<double> rnd(min_x, max_x);
     std::default_random_engine re;
 
     for (int i = 0; i < point_count; i++) {
         // fix y coordinate at 0 as there is no Point_1 class. 
-        points[i] = Point_2(rnd(re), 0);
+        points->push_back(Point_2(rnd(re), 0));
         #ifdef DEBUG_PRINTING
-        std::cout << "p" << i << ": " << points[i] << std::endl;
+        std::cout << "p" << i << ": " << (*points)[i] << std::endl;
         #endif
     }
+}
+
+bool less_than(Point_2 p1, Point_2 p2) {
+    return p1.x() < p2.x();
 }
 
 int main()
 {
     const int point_count = 10;
-    Point_2 points[point_count];
-    get_random_points(points, point_count);
+    std::vector<Point_2> points;
+    get_random_points(&points, point_count);
+    std::sort(points.begin(), points.end(), &less_than);
 
-    std::vector<int> list = { 0, 1, 2, 3, 4, 5, 6, 7 };
-    auto tree = DS::generate_tree<int>(list);
+    auto tree = DS::generate_tree<Point_2>(points, [](Point_2 p1, Point_2 p2) { return Point_2(p1.x() / 2 + p2.x() / 2, 0);  });
+    DS::print_tree<Point_2>(tree);
+    std::vector<DS::Tree<Point_2>*> search_path;
+    auto res = DS::query_tree<Point_2>(tree, Point_2(100, 0), &less_than, &search_path);
+    std::vector<int> count_copies;
+    for (auto i = search_path.begin(); i < search_path.end(); i++) {
+        (*i)->blocked = DS::Split;
+        // search path counts are set to 1 bc of unbalanced tree
+        // should be reset later
+        count_copies.push_back((*i)->count);
+        if (i == search_path.begin()) (*i)->count = (*i)->right->count + 1;
+        else (*i)->count = 1;
+    }
+
+    // for root_le, switch right and left subtree if needed
+    auto root_le = tree, root_geq = tree;
+    unsigned int k = 5;
+
+    // blue == le
+    while (root_le->type != DS::Leaf && root_geq->type != DS::Leaf) {
+        unsigned int geq_count = root_geq->blocked == DS::Left ? 0 : root_geq->left->count;
+        unsigned int l = root_le->right->count + geq_count + 1;
+        if (l <= k) {
+            // update pointer for the blue (le) root to go to the right (actually left)
+            root_le = root_le->left;
+            // Then, block off the left subtree of the red (geq) root
+            root_geq -> blocked = DS::Left;
+            // Dit moet even gereset worden later; wss iets met een array aan pointers samen met de rest van de counts;
+            root_geq->count = root_geq->count - root_geq->right->count;
+
+            k = k - l;
+        }
+        else {
+            // nu alleen rode tree update :3
+            root_geq = root_geq->right;
+        }
+    }
+
     DS::cleanup_tree(tree);
     
     std::string str;
