10ed7df5d4d095890a29a6ae9b2f1ddc254f54ba
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2024-05-01 20:13:47 +0200

    correct counts after splits

--- src/datastructures/tree.cpp
+++ src/datastructures/tree.cpp
@@ -13,15 +13,26 @@ namespace DS {
         Left,
         Split,
     };
+    enum Direction {
+        L, 
+        R,
+    };
 
     template<class T>
     struct Tree {
         NodeType type;
         T value;
         unsigned int count;
-        Blocked blocked;
+        int left_count = -1;
+        int right_count = -1;
         Tree<T>* left;
         Tree<T>* right;
+    };
+
+    template<class T>
+    struct SearchNode {
+        Tree<T>* node;
+        Direction direction;
     };
 
     template<class T>
@@ -39,6 +50,7 @@ namespace DS {
             leaf->type = Leaf;
             leaf->value = entries[begin];
             leaf->count = 1;
+            leaf->left_count = leaf->right_count = -1;
             return leaf;
         }
         else {
@@ -49,6 +61,7 @@ namespace DS {
             node->right = generate_tree(entries, between, middle, end);
             node->value = between(node->left->value, node->right->value);
             node->count = node->left->count + node->right->count;
+            node->left_count = node->right_count = -1;
             return node;
         }
     }
@@ -63,15 +76,56 @@ namespace DS {
         }
     }
 
-    template<class T>
-    // Returns the first node in the tree which is larger than the 
-    // provided query value, determined by the predicate.
-    // Also outputs the entire search path 
-    static Tree<T>* query_tree(Tree<T>* tree, T query_value, std::function<bool(T, T)> pred, std::vector<Tree<T>*>* search_path) {
-        search_path->push_back(tree);
-        if (tree->type == Leaf) return tree;
-        auto target = pred(query_value, tree->value) ? tree->left : tree->right;
-        return query_tree(target, query_value, pred, search_path);
+    template <class T>
+    static std::vector<SearchNode<T>> query_tree(Tree<T>* tree, T query_value, std::function<bool(T, T)> pred) {
+        SearchNode<T> root;
+        root.node = tree;
+
+        std::vector<SearchNode<T>> search_path = { root };
+        while (tree->type != Leaf) {
+            if (pred(query_value, tree->value)) {
+                (*(search_path.end() - 1)).direction = L;
+                tree = tree->left;
+            }
+            else {
+                (*(search_path.end() - 1)).direction = R;
+                tree = tree->right;
+            }
+            SearchNode<T> new_node;
+            new_node.node = tree;
+            search_path.push_back(new_node);
+        }
+        (*(search_path.end() - 1)).direction = pred(query_value, tree->value) ? L : R;
+        return search_path;
+    }
+
+    template <class T>
+    static void update_tree_counts(Tree<T>* tree, std::vector<SearchNode<T>> search_tree) {
+        auto root = tree;
+
+        // First, create counts for the left split
+        for (int i = (int)search_tree.size() - 1; i >= 0; i--) {
+            if (search_tree[i].node->type == Leaf) {
+                search_tree[i].node->left_count = search_tree[i].direction == L ? 0 : 1;
+            }
+            else {
+                auto left_contribution = search_tree[i].node->left->left_count == -1 ? search_tree[i].node->left->count : search_tree[i].node->left->left_count;
+                auto right_contribution = search_tree[i].direction == R ? search_tree[i].node->right->left_count : 0;
+                search_tree[i].node->left_count = left_contribution + right_contribution;
+            }
+        }
+
+        // Then, basically do the same thing for the right split but in reverse
+        for (int i = (int)search_tree.size() - 1; i >= 0; i--) {
+            if (search_tree[i].node->type == Leaf) {
+                search_tree[i].node->right_count = search_tree[i].direction == R ? 0 : 1;
+            }
+            else {
+                auto right_contribution = search_tree[i].node->right->right_count == -1 ? search_tree[i].node->right->count : search_tree[i].node->right->right_count;
+                auto left_contribution = search_tree[i].direction == L ? search_tree[i].node->left->right_count : 0;
+                search_tree[i].node->right_count = left_contribution + right_contribution;
+            }
+        }
     }
 
     template<class T>
--- src/main.cpp
+++ src/main.cpp
@@ -12,74 +12,27 @@ typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_2 Point_2;
 typedef Kernel::Segment_2 Segment_2;
 
-void get_random_points(std::vector<Point_2>* points, int point_count) {
-    const int min_x = -1000;
-    const int max_x = 1000;
-    std::uniform_real_distribution<double> rnd(min_x, max_x);
-    std::default_random_engine re;
-
-    for (int i = 0; i < point_count; i++) {
-        // fix y coordinate at 0 as there is no Point_1 class. 
-        points->push_back(Point_2(rnd(re), 0));
-        #ifdef DEBUG_PRINTING
-        std::cout << "p" << i << ": " << (*points)[i] << std::endl;
-        #endif
-    }
-}
-
-bool less_than(Point_2 p1, Point_2 p2) {
-    return p1.x() < p2.x();
-}
+bool less_than(double a, double b) { return a < b; }
+double between(double a, double b) { return a / 2 + b / 2;  }
 
 int main()
 {
-    const int point_count = 10;
-    std::vector<Point_2> points;
-    get_random_points(&points, point_count);
-    std::sort(points.begin(), points.end(), &less_than);
+    // First, create tree
+    std::vector<double> points = { 1, 4, 7, 8, 10, 12, 13, 18 };
+    DS::Tree<double>* tree = DS::generate_tree<double>(points, &between);
+#ifdef DEBUG_PRINTING
+    DS::print_tree(tree);
+#endif // DEBUG_PRINTING
 
-    auto tree = DS::generate_tree<Point_2>(points, [](Point_2 p1, Point_2 p2) { return Point_2(p1.x() / 2 + p2.x() / 2, 0);  });
-    DS::print_tree<Point_2>(tree);
-    std::vector<DS::Tree<Point_2>*> search_path;
-    auto res = DS::query_tree<Point_2>(tree, Point_2(100, 0), &less_than, &search_path);
-    std::vector<int> count_copies;
-    for (auto i = search_path.begin(); i < search_path.end(); i++) {
-        (*i)->blocked = DS::Split;
-        // search path counts are set to 1 bc of unbalanced tree
-        // should be reset later
-        count_copies.push_back((*i)->count);
-        if (i == search_path.begin()) (*i)->count = (*i)->right->count + 1;
-        else (*i)->count = 1;
-    }
+    // Then, do query for q = 5 with k = 3
+    double q = 5, k = 3;
+    auto search_path = DS::query_tree<double>(tree, q, &less_than);
+    DS::update_tree_counts<double>(tree, search_path);
 
-    // for root_le, switch right and left subtree if needed
-    auto root_le = tree, root_geq = tree;
-    unsigned int k = 5;
-
-    // blue == le
-    while (root_le->type != DS::Leaf && root_geq->type != DS::Leaf) {
-        unsigned int geq_count = root_geq->blocked == DS::Left ? 0 : root_geq->left->count;
-        unsigned int l = root_le->right->count + geq_count + 1;
-        if (l <= k) {
-            // update pointer for the blue (le) root to go to the right (actually left)
-            root_le = root_le->left;
-            // Then, block off the left subtree of the red (geq) root
-            root_geq -> blocked = DS::Left;
-            // Dit moet even gereset worden later; wss iets met een array aan pointers samen met de rest van de counts;
-            root_geq->count = root_geq->count - root_geq->right->count;
-
-            k = k - l;
-        }
-        else {
-            // nu alleen rode tree update :3
-            root_geq = root_geq->right;
-        }
-    }
-
-    DS::cleanup_tree(tree);
-    
+#pragma region cleanup
     std::string str;
     std::cin >> str;
     return 0;
+#pragma endregion
 }
 
