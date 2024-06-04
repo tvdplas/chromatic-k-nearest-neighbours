83e3eb9dcebaadada9575868a2678446a0f3cee0
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2024-06-04 22:40:42 +0200

    added data visualisation

--- scripts/fetch_weather_stations.js
+++ scripts/fetch_weather_stations.js
@@ -1,26 +1,37 @@
 const fs = require("fs")
 const path = require("path")
 
+const DATE = "02-06-2024"
+const DATE_FORMATTED = DATE.replaceAll("-", "%2F")  
+
 const bins = [
   { lower: -200, upper: -50 },
   { lower: -50, upper: -40 },
   { lower: -30, upper: -20 },
   { lower: -20, upper: -10 },
-  { lower: -10, upper: 0 },
-  { lower: 0, upper: 5 },
-  { lower: 5, upper: 10 },
-  { lower: 10, upper: 15 },
-  { lower: 15, upper: 20 },
-  { lower: 20, upper: 25 },
-  { lower: 25, upper: 30 },
+  { lower: -10, upper: -5 },
+  { lower: -5, upper: 0 },
+  { lower: 0, upper: 2.5 },
+  { lower: 2.5, upper: 5 },
+  { lower: 5, upper: 7.5 },
+  { lower: 7.5, upper: 10 },
+  { lower: 10, upper: 12.5 },
+  { lower: 12.5, upper: 15 },
+  { lower: 15, upper: 17.5 },
+  { lower: 17.5, upper: 20 },
+  { lower: 20, upper: 22.5 },
+  { lower: 22.5, upper: 25 },
+  { lower: 25, upper: 27.5 },
+  { lower: 27.5, upper: 30 },
   { lower: 30, upper: 35 },
   { lower: 35, upper: 40 },
   { lower: 40, upper: 50 },
   { lower: 50, upper: 200 },
 ]
 
+
 const f = async () => {
-  const res = await fetch("https://wowapi.metweb.ie/getObservations/dry_bulb?date=02%2F06%2F2024&time=20&timePoint=-1&mapFilterTags=&showWowData=true&showOfficialData=true&showRegisteredSites=false");
+  const res = await fetch(`https://wowapi.metweb.ie/getObservations/dry_bulb?date=${DATE_FORMATTED}&time=20&timePoint=-1&mapFilterTags=&showWowData=true&showOfficialData=true&showRegisteredSites=false`);
   const data = JSON.parse(await new Response(res.body).text())
 
   const lines = data.features.map(feat => {
@@ -30,7 +41,7 @@ const f = async () => {
     return `${feat.geometry.coordinates[0]};${feat.geometry.coordinates[1]};${bin}`
   })
 
-  fs.writeFileSync(path.resolve("../data/temperature/data.txt"), lines.filter(x => x.length).join("\n"))
+  fs.writeFileSync(path.resolve(`../data/temperature-${DATE}.points`), lines.filter(x => x.length).join("\n"))
 }
 
 f();
---
+++ scripts/visualize.js
@@ -0,0 +1,47 @@
+const {Image} = require('imagescript');
+const fs = require('fs')
+const path = require('path')
+
+const NAME = "temperature-02-06-2024"
+const WIDTH = 3600
+const HEIGHT = 1800
+const MIN_X_VAL = -180
+const MAX_X_VAL = 180
+const MIN_Y_VAL = -90
+const MAX_Y_VAL = 90
+
+const run = async () => {
+    const pointsCsv = fs.readFileSync(path.resolve(`../data/${NAME}.points`)).toString()
+    const points = pointsCsv.split("\n").map(line => {
+      const items = line.split(";")
+      return { 
+        x: parseFloat(items[0]),
+        y: parseFloat(items[1]),
+        c: parseInt(items[2]),
+      }
+    })
+
+    const colorsCount = Math.max(...points.map(p => p.c)) + 1
+    const colors = Array.from({length: colorsCount + 1}).map(_ => {
+      const color = Math.random() * Math.pow(2, 24)
+      return color << 8 + 0xFF;
+    })
+
+    const image = new Image(WIDTH, HEIGHT);
+    image.drawBox(0, 0, WIDTH, HEIGHT, 0xFFFFFFFF);
+
+    points.forEach( point => 
+      image.drawBox(
+        Math.round((point.x - MIN_X_VAL) / (MAX_X_VAL - MIN_X_VAL) * WIDTH),
+        HEIGHT - Math.round((point.y - MIN_Y_VAL) / (MAX_Y_VAL - MIN_Y_VAL) * HEIGHT),
+        4, 4, colors[point.c]
+      )
+    )
+      
+
+    const encoded = await image.encode()
+
+    await fs.writeFileSync(`../figs/renders/${NAME}.png`, encoded);
+}
+
+run();
